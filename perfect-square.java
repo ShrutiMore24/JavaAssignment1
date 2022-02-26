134fa28fca9389bbc1558ab98f4b7205c658e79c
ShrutiMore24 <97089262+ShrutiMore24@users.noreply.github.com>
2022-02-26 16:54:16 +0530

    Add files via upload

---
+++ perfect-square.java
@@ -0,0 +1,7 @@
+class perfectsquare    {    
+    public static void main(String[] args)     
+    {    
+        double y = 4;    
+        System.out.println("Square root of y is: " + Math.sqrt(y));
+    }
+}
